c4dbb89993f091f13d39182a2aa5dc5f0c9f0090
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-12 15:54:32 +0530

    learnt how to add the icons in the menu list in the menu using the mataerial ui

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,24 @@
 // Test -------------------------- Importing the Packages ---------------------------------
-import React from "react";
+import { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Button,
+  IconButton,
+  Tooltip,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import FeedIcon from "@mui/icons-material/Feed";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 // Test -------------------------- Importing the styles / other components ----------------
 
@@ -7,7 +26,82 @@ import React from "react";
 
 // Test -------------------------- The current component ----------------------------------
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorElement);
+
+  const avatarMenuOpenClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElement(event?.currentTarget);
+  };
+
+  const avatarMenuCloseClick = () => {
+    setAnchorElement(null);
+  };
+
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar sx={{ backgroundColor: "#67568c" }}>
+          <Button
+            variant="contained"
+            sx={{ color: "white" }}
+            startIcon={<VpnKeyIcon sx={{ color: "white" }}></VpnKeyIcon>}
+          >
+            Vault
+          </Button>
+          <Tooltip title="See all the blogs" arrow>
+            <IconButton>
+              <FeedIcon sx={{ color: "white" }}></FeedIcon>
+            </IconButton>
+          </Tooltip>
+
+          <Button sx={{ color: "white" }}>Sign Up</Button>
+          <Button sx={{ color: "white" }}>Login</Button>
+          <Button sx={{ color: "white" }} endIcon={<LogoutIcon></LogoutIcon>}>
+            Logout
+          </Button>
+
+          <Tooltip title="Click to get different menu items">
+            <>
+              <IconButton onClick={avatarMenuOpenClick}>
+                <Avatar
+                  src="https://mui.com/static/images/avatar/2.jpg"
+                  alt="Ansh Singh"
+                ></Avatar>
+              </IconButton>
+
+              <Menu
+                open={open}
+                anchorEl={anchorElement}
+                onClose={avatarMenuCloseClick}
+                disableScrollLock
+              >
+                <MenuItem onClick={avatarMenuCloseClick}>
+                  <ListItemIcon>
+                    <AccountCircleIcon></AccountCircleIcon>
+                  </ListItemIcon>
+                  <ListItemText>Profile</ListItemText>
+                </MenuItem>
+
+                <MenuItem onClick={avatarMenuCloseClick}>
+                  <ListItemIcon>
+                    <SettingsIcon></SettingsIcon>
+                  </ListItemIcon>
+                  <ListItemText>Settings</ListItemText>
+                </MenuItem>
+
+                <MenuItem onClick={avatarMenuCloseClick}>
+                  <ListItemIcon>
+                    <LogoutIcon></LogoutIcon>
+                  </ListItemIcon>
+                  <ListItemText>Logout</ListItemText>
+                </MenuItem>
+              </Menu>
+            </>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 // Test -------------------------- Exporting the current component ------------------------
