b7ffd812711bb0a244ca22cbf00238d7cf46e22e
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-14 01:31:08 +0530

    The UI looks very good for the web, but not responsive for the phones, so need to think about that, and also ablel to add the image with the help of the BOx

--- client/src/components/body/signup/Signup.tsx
+++ client/src/components/body/signup/Signup.tsx
@@ -26,75 +26,77 @@ import {
 const Signup = () => {
   const navigate = useNavigate();
   return (
-    <Box
-      sx={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translateX(-50%) translateY(-50%)",
-      }}
-    >
-      <Card sx={{ maxWidth: "400px", m: "auto", mt: 4, mb: 4 }}>
-        <CardHeader
-          title={<Typography variant="h6">Welcome new user</Typography>}
-          subheader="Create a new account to explore more"
-          sx={{ textAlign: "left", pb: 0 }}
-        ></CardHeader>
+    <Grid container justifyContent="center" columnSpacing={4} mt={4} mb={4}>
+      <Grid item xs={4}>
+        <Box
+          component="img"
+          src="https://res.cloudinary.com/dicbnntfh/image/upload/v1657720062/signup_ni5ytq.svg"
+          alt="Test"
+        ></Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Card sx={{ maxWidth: "400px", m: "auto" }}>
+          <CardHeader
+            title={<Typography variant="h6">Welcome new user</Typography>}
+            subheader="Create a new account to explore more"
+            sx={{ textAlign: "left", pb: 0 }}
+          ></CardHeader>
 
-        <Grid container spacing={1} justifyContent="space-evenly">
-          <Grid item xs={12}>
-            <CardContent>
-              <Box display="flex" flexDirection="column" gap={1.5}>
-                <TextField label="Username"></TextField>
-                <TextField label="Password"></TextField>
-                <TextField label="Confirm Password"></TextField>
-              </Box>
-              <CardActions sx={{ pl: 0, pr: 0, mt: 1 }}>
-                <Button fullWidth variant="contained" sx={{ m: 0 }}>
-                  Submit
-                </Button>
-              </CardActions>
-
-              <CardActionArea
-                onClick={() => {
-                  navigate("/login");
-                }}
-              >
-                <CardHeader
-                  subheader="Already have an Account ? Login"
-                  sx={{ pt: 0.5, pb: 0.5, textAlign: "center" }}
-                ></CardHeader>
-              </CardActionArea>
-
-              <Divider orientation="horizontal" sx={{ mt: 1 }}>
-                OR
-              </Divider>
-              <Grid container rowSpacing={1.5} columnSpacing={1.5} mt={0.75}>
-                <Grid item xs={12}>
-                  <GoogleButton variant="contained" fullWidth>
-                    Google
-                  </GoogleButton>
-                </Grid>
-                <Grid item xs={6}>
-                  <Button variant="outlined" fullWidth>
-                    Facebook
+          <Grid container spacing={1} justifyContent="space-evenly">
+            <Grid item xs={12}>
+              <CardContent>
+                <Box display="flex" flexDirection="column" gap={1.5}>
+                  <TextField label="Username"></TextField>
+                  <TextField label="Password"></TextField>
+                  <TextField label="Confirm Password"></TextField>
+                </Box>
+                <CardActions sx={{ pl: 0, pr: 0, mt: 1 }}>
+                  <Button fullWidth variant="contained" sx={{ m: 0 }}>
+                    Submit
                   </Button>
+                </CardActions>
+
+                <CardActionArea
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  <CardHeader
+                    subheader="Already have an Account ? Login"
+                    sx={{ pt: 0.5, pb: 0.5, textAlign: "center" }}
+                  ></CardHeader>
+                </CardActionArea>
+
+                <Divider orientation="horizontal" sx={{ mt: 1 }}>
+                  OR
+                </Divider>
+                <Grid container rowSpacing={1.5} columnSpacing={1.5} mt={0.75}>
+                  <Grid item xs={12}>
+                    <GoogleButton variant="contained" fullWidth>
+                      Google
+                    </GoogleButton>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Button variant="outlined" fullWidth>
+                      Facebook
+                    </Button>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <GithubButton
+                      variant="outlined"
+                      fullWidth
+                      sx={{ "&:hover": { backgroundColor: "grey" } }}
+                    >
+                      Github
+                    </GithubButton>
+                  </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                  <GithubButton
-                    variant="outlined"
-                    fullWidth
-                    sx={{ "&:hover": { backgroundColor: "grey" } }}
-                  >
-                    Github
-                  </GithubButton>
-                </Grid>
-              </Grid>
-            </CardContent>
+              </CardContent>
+            </Grid>
           </Grid>
-        </Grid>
-      </Card>
-    </Box>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
