d96b1c2815256c383f5f1928d24d21071ca06297
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-14 16:49:01 +0530

    Finally able to make the good looking home page UI with the help of the Material UI, and also added the Link to the /blogs route, now only need to make the loader

--- client/src/components/body/home/Home.tsx
+++ client/src/components/body/home/Home.tsx
@@ -1,6 +1,6 @@
 // Test -------------------------- Importing the Packages ---------------------------------
-import { Grid, Box } from "@mui/material";
-
+import { Grid, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 // Test -------------------------- Importing the styles / other components ----------------
 import BlogCard from "./BlogCard";
 
@@ -8,53 +8,75 @@ import BlogCard from "./BlogCard";
 
 // Test -------------------------- The current component ----------------------------------
 const Home = () => {
+  let navigate = useNavigate();
+
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-      mt={4}
-      mb={4}
-    >
-      <Grid item xs={4}>
-        <BlogCard></BlogCard>
-      </Grid>
+    <>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        pt={4}
+        pb={4}
+        sx={{ backgroundColor: "aliceblue" }}
+      >
+        <Grid item xs={4}>
+          <BlogCard></BlogCard>
+        </Grid>
 
-      <Grid item xs={2.5}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          gap={2}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item>
-            <BlogCard removeImage={true}></BlogCard>
-          </Grid>
-          <Grid item>
-            <BlogCard removeImage={true}></BlogCard>
-          </Grid>
-        </Box>
-      </Grid>
+        <Grid item xs={2.5}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <BlogCard removeImage={true}></BlogCard>
+            </Grid>
+            <Grid item>
+              <BlogCard removeImage={true}></BlogCard>
+            </Grid>
+          </Box>
+        </Grid>
 
-      <Grid item xs={2.5}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          gap={2}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item>
-            <BlogCard removeImage={true}></BlogCard>
-          </Grid>
-          <Grid item>
-            <BlogCard removeImage={true}></BlogCard>
-          </Grid>
-        </Box>
+        <Grid item xs={2.5}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <BlogCard removeImage={true}></BlogCard>
+            </Grid>
+            <Grid item>
+              <BlogCard removeImage={true}></BlogCard>
+            </Grid>
+          </Box>
+        </Grid>
       </Grid>
-    </Grid>
+      <Grid
+        container
+        justifyContent="center"
+        sx={{ backgroundColor: "aliceblue" }}
+        pb={4}
+      >
+        <Grid item xs={9} height="100px">
+          <Button
+            fullWidth
+            variant="outlined"
+            sx={{ height: "100%" }}
+            onClick={() => navigate("/blogs")}
+          >
+            Explore More Blogs
+          </Button>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
