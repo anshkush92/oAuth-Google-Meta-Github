2084927eba9b11e19acc28aaba4f798ac43c3976
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-16 15:56:10 +0530

    Learnt how to avoid creating duplicated user, by first checking whether user exists or not as googleId, doesn't change for user ----> Net Ninja vn 13

--- backend/config/passportSetup.js
+++ backend/config/passportSetup.js
@@ -14,6 +14,17 @@ passport.use(new googleStrategy({
     // Function which gets fired up when we are redirected to /auth/google/callback
     // This function is used to exchange the code for the profile information 
     console.log("Passport callback function fired");
-    const newGoogleUser = await GoogleUser.create({ googleId: profile.id, displayName: profile.displayName, avatar: profile.photos[0].value });
-    console.log(`New Google User is ---> ${newGoogleUser}`);
+
+    // SO when this is fired, first we will look for that id in the database and if not ever logged in then create a new user
+    const oldGoogleUser = await GoogleUser.findOne({ googleId: profile.id });
+
+    // Checking for User, so that we don't end up adding duplicate users
+    if (oldGoogleUser) {
+        // The user has already used the Google to sign in 
+        console.log(`User already exists in the database ${oldGoogleUser}`);
+    } else {
+        // Adding the new google User
+        const newGoogleUser = await GoogleUser.create({ googleId: profile.id, displayName: profile.displayName, avatar: profile.photos[0].value });
+        console.log(`New Google User is ---> ${newGoogleUser}`);
+    }
 }))
