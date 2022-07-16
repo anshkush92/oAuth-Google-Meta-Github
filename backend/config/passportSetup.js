eba229244c58649e2f06c793f5b45653bf6e0d77
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-16 16:24:29 +0530

    Learnt how to serializeUser and deserializeUser using the passportJs ---> Net Ninja vn 15

--- backend/config/passportSetup.js
+++ backend/config/passportSetup.js
@@ -3,6 +3,23 @@ const passport = require("passport");
 const GoogleUser = require("../models/googleModel");
 const googleStrategy = require("passport-google-oauth20").Strategy;
 
+// This functions takes the user information from the database, and encrypt cookie with the information
+passport.serializeUser((user, done) => {
+    console.log(`Serialized User by encrypting cookie with the ${user.id}`);
+    // Using the mongoDB id created to encrypt the cookie to tell that the user is authenticated
+    // done () ---> similar to what next() does
+    done(null, user.id);
+});
+
+// This functions takes the cookie and deserialize and then looks up for the info in the database
+passport.deserializeUser(async (id, done) => {
+    // Using the mongoDB id created to encrypt the cookie to tell that the user is authenticated
+    const findUser = await GoogleUser.findById(id);
+    console.log(`User found after de-serializing the cookie --> ${findUser}`);
+    done(null, findUser);
+});
+
+
 passport.use(new googleStrategy({
     // Options for the google strategy
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -22,9 +39,13 @@ passport.use(new googleStrategy({
     if (oldGoogleUser) {
         // The user has already used the Google to sign in 
         console.log(`User already exists in the database ${oldGoogleUser}`);
+        // Passing the oldGoogleUser data in MongoDb to the Serialize user, which will create a encrypted cookie with this info
+        done(null, oldGoogleUser);
     } else {
         // Adding the new google User
         const newGoogleUser = await GoogleUser.create({ googleId: profile.id, displayName: profile.displayName, avatar: profile.photos[0].value });
         console.log(`New Google User is ---> ${newGoogleUser}`);
+        // Passing the oldGoogleUser data in MongoDb to the Serialize user, which will create a encrypted cookie with this info
+        done(null, newGoogleUser);
     }
 }))
