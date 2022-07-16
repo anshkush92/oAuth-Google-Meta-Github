338fe7a758251760daea646778f447834a9ac0af
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-16 14:35:20 +0530

    Write the options for the google strategy which it will be using by seeing the document and net ninja video no 6

--- backend/config/passportSetup.js
+++ backend/config/passportSetup.js
@@ -3,6 +3,10 @@ const googleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(new googleStrategy({
     // Options for the google strategy
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8000/auth/google/callback"
+
 }), () => {
     // Passport callback function 
 })
